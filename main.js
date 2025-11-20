5a20fd1953b829ad66eb86587d23f3f073638a04
Lusseni Kaba <lusseni.kaba@cesjb.be>
2025-11-20 15:59:46 +0100

    Complet

---
+++ main.js
@@ -0,0 +1,8 @@
+import Etudiant from "./Etudiant";
+
+var user1 = new Etudiant("YesKing",17,"info")
+
+var user2 = new Etudiant("lusseni",16,"info")
+
+var user3 = new Etudiant("Adrien",18,"latin")
+
